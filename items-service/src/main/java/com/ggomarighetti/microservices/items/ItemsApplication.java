8000b4ce350e768de5edc42fb3ca566a78404c79
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-09 18:26:20 -0300

    Implemented eureka server on services

--- items-service/src/main/java/com/ggomarighetti/microservices/items/ItemsApplication.java
+++ items-service/src/main/java/com/ggomarighetti/microservices/items/ItemsApplication.java
@@ -2,14 +2,16 @@ package com.ggomarighetti.microservices.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class ItemsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItemsApplication.class, args);
+    }
 
 }
