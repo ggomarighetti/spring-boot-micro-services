f1d69e94c2e860fdb3619cfe9d0a57345f14ed73
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-09 16:02:52 -0300

    Updated application properties

--- items-service/src/main/java/com/ggomarighetti/microservices/items/ItemsApplication.java
+++ items-service/src/main/java/com/ggomarighetti/microservices/items/ItemsApplication.java
@@ -2,8 +2,10 @@ package com.ggomarighetti.microservices.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ItemsApplication {
 
 	public static void main(String[] args) {
