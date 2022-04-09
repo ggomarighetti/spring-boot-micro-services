8a9c929e60a11481f732e13aa0a280b13ea1daec
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-09 16:03:01 -0300

    Created item service

---
+++ items-service/src/main/java/com/ggomarighetti/microservices/items/service/ItemServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ggomarighetti.microservices.items.service;
+
+import com.ggomarighetti.microservices.items.client.ProductClient;
+import com.ggomarighetti.microservices.items.entity.ItemEntity;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ItemServiceImpl implements  ItemService {
+
+    @Autowired
+    private ProductClient productClient;
+
+    @Override
+    public List<ItemEntity> findAll() {
+        return productClient.findAll().stream().map(p -> ItemEntity.builder().product(p).quantity(1).build()).collect(Collectors.toList());
+    }
+
+    @Override
+    public ItemEntity findById(String id) {
+        return ItemEntity.builder().product(productClient.findById(id)).quantity(1).build();
+    }
+}
